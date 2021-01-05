285895078dde2f4c7033a50b69ce3c1b36755992
jinsw11 <73919405+jinsw11@users.noreply.github.com>
2021-01-05 23:34:47 +0900

    Delete SolarCharger.js

--- SolarCharger.js
+++
@@ -1,8 +0,0 @@
-function idea_list() {
-    var coworkers = ['날개', '2', 'me', 'you']
-            var i = 0;
-            while (i < 4) {
-                document.write('<li>' + coworkers[i] + '</li>')
-                i += 1
-            }
-}
